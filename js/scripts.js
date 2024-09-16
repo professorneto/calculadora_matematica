52c608c1831e71bfe27ca80fe016c4367b9162fa
Professor Neto <professorneto2023@gmail.com>
2024-09-16 20:16:05 -0300

    Aula 16-09

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,23 @@
 $(document).ready(function() {
-    
+
+    // $('#txt_display').mask('000\'000\'000\'000\'000', {reverse: true});
+
     var bt_nr = '';
 
     $('.bt-nr').click(function() {
         var n = $(this).text();
 
-        if(bt_nr[0] !== '0') {
+        if( n == '0' || n == '00' ) {
 
-            bt_nr = bt_nr.concat(n);
-
-        } else if (bt_nr[0] !== '0' && bt_nr[1] !== '.') {
-
-            bt_nr = bt_nr.concat(n);
-
-        } else {
-            
-            if(bt_nr.length == 1 && n == '.') {
-
-                bt_nr = bt_nr.concat(n);
-                $(this).attr('disabled', 'true');
-
-            } else {
+            if( bt_nr.length > 0 ) {
 
                 bt_nr = bt_nr.concat(n);
 
             }
+
+        } else {
+
+            bt_nr = bt_nr.concat(n);
 
         }
 
